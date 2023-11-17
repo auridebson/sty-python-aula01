b60567e9ee18e754b97b4321525c764d119f2e4b
Auridebson Lima <auridebson@vutus.com>
2023-11-17 10:57:06 -0300

    Update

---
+++ operacao_numeros.py
@@ -0,0 +1,7 @@
+6 - Faça um Programa que receba 2 números e em seguida pergunte ao usuário qual
+operação ele deseja realizar. O resultado da operação deve aparecer com uma frase que
+diga se o número é:
+4 - Faça um Programa que verifique se a letra digitada pelo usuário é vogal ou consoante.
+5 - Faça um programa para a leitura de quatro notas de um aluno. O programa deve calcular a média alcançada apresentar:
+a. par ou ímpar;
+b. positivo ou negativo;
