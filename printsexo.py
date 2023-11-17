b64a49b0f5631c0efb347d33350c37143304dc8d
Auridebson Lima <auridebson@vutus.com>
2023-11-17 09:54:45 -0300

    Update

--- printsexo.py
+++ printsexo.py
@@ -5,12 +5,18 @@ def ln(x):
 # Feminimo - F
 # Masculino - M
 
-sexo = input("Digite o seu sexo.\n[M] - Masculino\n[F] - Feminino\n")
+sexo = input("Digite o seu sexo.\n   [M] - Masculino\n   [F] - Feminino\n")
 
 if sexo.upper() == 'M':
+    ln(30)
     print("Sexo: Masculino escolhido")
+    ln(30)
 elif sexo.upper() == 'F':
+    ln(30)
     print("Sexo: Feminino escolhido")
+    ln(30)
 else:
+    ln(30)
     print("ERROR:\nSexo Inválido!")
+    ln(30)
 
